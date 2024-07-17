ade1581b63e96013233428c6b11a13f16d676ba1
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2024-07-17 20:12:12 +0530

    Development on UART

--- Libraries/UART/UART/uart.c
+++ Libraries/UART/UART/uart.c
@@ -1,9 +1,21 @@
 /*
- * Usart328P.c
+ * uart328P.h
  *
- * Created: 30-Oct-18 6:53:44 PM
- * Author : Ranul Deepanayake
+ * Created: 30-Oct-18 6:54:33 PM
+ * Author: Ranul Deepanayake.
+ * UART library for the ATmega328P. Supports character transmission, reception, selectable baud rates, reception error detection and activity indication.
+ * Supports baud rates of 9600- 57600 (other values may work but have not been tested).
+ * Circular buffer and interrupt based transmission and reception. Tx and Rx buffers sizes can be set individually. 
+ * Supports 5- 8 bit data frames. Development for 9 bit data frames is pending.
+ * Supports parities of none, even and odd.
+ * Supports one and two stop bits.
+ * Supports frame error, overrun error and parity error detection with free running error counters.
+ * Supports Tx/Rx activity indicator LEDs which use PORTD. Default pins; Rx- PORTD2 and Tx- PORTD3.
  */ 
+
+//Includes.
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #include "uart.h"
 
 //Buffer for Tx.
@@ -29,15 +41,22 @@ typedef struct circular_buffer_rx{
 circular_buffer_tx _buffer_tx = { .head = 0, .tail = 0, .count = 0, .size = UART_TX_BUFFER_SIZE };
 circular_buffer_rx _buffer_rx = { .head = 0, .tail = 0, .count = 0, .size = UART_RX_BUFFER_SIZE };	
 
+//Error counters.
+volatile uint8_t _rx_frame_errors = 0, _rx_overrun_errors = 0, _rx_parity_errors = 0;
+
 typedef uint8_t USART_LED_STATUS;
 enum USART_LED_STATUS_OPTIONS {USART_LED_OFF, USART_LED_ON};
 	
 USART_LED_STATUS usart_led_toggle = USART_LED_OFF;
 
+/*
+This ISR is used to pop data from the Tx buffer and send it to the UART data register to be transmitted.
+*/
 ISR(USART_UDRE_vect){
 	usart_led_toggle == USART_LED_ON ? (PORTD |= UART_TX_LED) : (PORTD &= ~UART_TX_LED);		//Turn on RX LED.
 	char data = 0x00;
 	
+	//Pop only if data is in the buffer.
 	if(uartAvailable(UART_BUFFER_TYPE_TX)){
 		uartBufferPop(UART_BUFFER_TYPE_TX, &data);
 		UDR0= data;
@@ -48,57 +67,72 @@ ISR(USART_UDRE_vect){
 	usart_led_toggle== USART_LED_ON ? (PORTD &= ~UART_TX_LED) : (PORTD &= ~UART_TX_LED);	//Turn off TX LED. Used for resiliency against turning off the LEDs within interrupts.
 }
 
-//The ISR catches received characters and pushes them into the RX buffer.
+/*
+This ISR catches received characters and pushes them into the RX buffer. Error checking is also performed and error counters are incremented appropriately.
+*/
 ISR(USART_RX_vect){
 	usart_led_toggle == USART_LED_ON ? (PORTD |= UART_RX_LED) : (PORTD &= ~UART_RX_LED);		//Turn on RX LED.	
-	uartBufferPush(UART_BUFFER_TYPE_TX, UDR0);
+	
+	//Read error flags before reading UDR0.
+	if(uartFrameError()){
+		_rx_frame_errors++;
+	}
+	if(uartOverrunError()){
+		_rx_overrun_errors++;
+	}
+	if(uartParityError()){
+		_rx_parity_errors++;
+	}
+	
+	if(!uartIsFull(UART_BUFFER_TYPE_RX)){
+		uartBufferPush(UART_BUFFER_TYPE_RX, UDR0);
+	}
 	usart_led_toggle== USART_LED_ON ? (PORTD &= ~UART_RX_LED) : (PORTD &= ~UART_RX_LED);	//Turn off TX LED. Used for resiliency against turning off the LEDs within interrupts.
 }
 
 /*
-Initializes the UART peripheral. 
-A baud rate between 9600- 57600 is supported.
-Frames of 5- 8 data bits are supported.
+Initializes the UART peripheral to run on the asynchronous mode.
+A baud rate between 9600- 57600 is supported. Frames of 5- 8 data bits are supported.
+Parities of none, even and odd are supported. Enables UART and global interrupts.
 */
 void uartSet(uint16_t baud_rate, uint8_t data_bits, uint8_t parity, uint8_t stop_bits){
 	cli();	//Disable global interrupts before setting up the UART peripheral.
 	
 	//Set the UART registers.
+	UCSR0B |= (UART_RX_ENABLE | UART_TX_ENABLE); //Enable Tx and Rx.
+	UCSR0B |= (UART_RX_INTERRUPT_ENABLE); //Enable the necessary interrupts.
+	UCSR0C |= (UART_ASYNCHRONOUS_MODE | parity | stop_bits);	//Set mode, parity and stop bits.
 	UBRR0H = baud_rate >> 8;	//Set the baud rate high byte.
-	UBRR0L = baud_rate;	//Set the baud rate low byte.
-	UCSR0B = (UART_RX_ENABLE | UART_TX_ENABLE); //Enable Tx and Rx.
-	UCSR0B |= (UART_RX_INTERRUPT_ENABLE); //Enable the necessary interrupts. 		
+	UBRR0L = baud_rate;			//Set the baud rate low byte. 		
 	
 	//Set frame size.
-	/*switch(data_bits){
-		case 5:
-			UCSR0C = UART_DATA_SIZE_5
+	switch(data_bits){
+		case UART_DATA_SIZE_5:
+			UCSR0C |= UART_DATA_SIZE_5;
 			break;
-		case 6:
-			UCSR0C = UART_DATA_SIZE_6
+		case UART_DATA_SIZE_6:
+			UCSR0C |= UART_DATA_SIZE_6;
 			break;
-		case 7:
-			UCSR0C = UART_DATA_SIZE_7
+		case UART_DATA_SIZE_7:
+			UCSR0C |= UART_DATA_SIZE_7;
 			break;
-		case 8:
-			UCSR0C = UART_DATA_SIZE_8
+		case UART_DATA_SIZE_8:
+			UCSR0C |= UART_DATA_SIZE_8;
 			break;
-		//case 9:
-		   //UCSR0C |= UART_DATA_SIZE_9
-		//break;
-	}*/
-	
-	//***Double check the below.
-	UCSR0C |= (UART_ASYNCHRONOUS_MODE | parity | stop_bits);	//Set mode, parity and stop bits.
-	UCSR0C |= UART_DATA_SIZE_8
-	UCSR0A &= (0<< U2X0);	//Set this bit to 0 to disable 2x mode.
-	
+		//case UART_DATA_SIZE_9:
+			//Need to configure two registers.
+			//UCSR0B |= 0x04;
+		    //UCSR0C |= UART_DATA_SIZE_8;
+			//break;
+		default:
+		    UCSR0C |= UART_DATA_SIZE_8;
+	}
 	
 	sei();	//Re-enable global interrupts.
 }
 
 /*
-Enables or disables UART activity LEDs. Default LEDs: RX- PORTD2 and TX- PORTD3.
+Enables or disables UART activity LEDs. Default LEDs: Rx- PORTD2 and Tx- PORTD3.
 */
 void uartSetLed(uint8_t toggle){
 	if(toggle == 1){
@@ -110,20 +144,8 @@ void uartSetLed(uint8_t toggle){
 }
 
 /*
-(Deprecated) Transmits a single character on the Tx line using polling.
-*/
-void uartSendChar(char data){
-	usart_led_toggle == USART_LED_ON ? (PORTD |= UART_TX_LED) : (PORTD &= ~UART_TX_LED);		//Turn on the Tx LED.
-	
-	while(!(UCSR0A & UART_DATA_REGISTER_EMPTY)); //Check the status of UDRE0. Transmit only when ready.
-	UDR0= data;	//Write data to the register.
-	//while(!(UCSR0A & (1 << TXC0))); //Wait till the shift register has shifted out all bits.
-	
-	usart_led_toggle == USART_LED_ON ? (PORTD &= ~UART_TX_LED) : (PORTD &= ~UART_TX_LED);	//Turn off TX LED. Used for resiliency against turning off the LEDs within interrupts.
-}
-
-/*
-Transmits a string of characters on the TX line. Carriage return and newline aren't sent.
+Loads a string of characters into the Tx buffer. Carriage return and newline aren't sent.
+An ISR will perform the transmission.
 */
 void uartPrint(char *string_pointer){
 	while((*string_pointer != UART_NULL_CHARACTER) && (!uartIsFull(UART_BUFFER_TYPE_TX))){ //Check for the null character in the string.
@@ -135,7 +157,8 @@ void uartPrint(char *string_pointer){
 }
 
 /*
-Transmits a string of characters on the TX line. Carriage return and newline are sent.
+Loads a string of characters with carriage return and newline into the Tx buffer.
+An ISR will perform the transmission.
 */
 void uartPrintLn(char *string_pointer){
 	uint8_t incrementer = 0;
@@ -156,8 +179,8 @@ void uartPrintLn(char *string_pointer){
 }
 
 /*
-Pushes a received character into the RX buffer.
-Called inside the USART_RX ISR. 
+Pushes a received character into the the selected buffer.
+Called inside the USART_RX_vect ISR. 
 */
 uint8_t uartBufferPush(uint8_t buffer_type, char data){
 	if(buffer_type == UART_BUFFER_TYPE_TX){
@@ -197,7 +220,8 @@ uint8_t uartBufferPush(uint8_t buffer_type, char data){
 }
 
 /*
-Pops a received character from the RX buffer.
+Pops a received character from the selected buffer.
+Called inside the USART_TX_vect ISR.
 */
 uint8_t uartBufferPop(uint8_t buffer_type, char *data){
 	if(buffer_type == UART_BUFFER_TYPE_TX){
@@ -237,7 +261,7 @@ uint8_t uartBufferPop(uint8_t buffer_type, char *data){
 }
 
 /*
-Returns if unread characters are present in the buffers.
+Returns if unread characters are present in the selected buffer.
 */
 uint8_t uartAvailable(uint8_t buffer_type){
 	if(buffer_type == UART_BUFFER_TYPE_TX){
@@ -258,7 +282,7 @@ uint8_t uartAvailable(uint8_t buffer_type){
 }
 
 /*
-Returns the number of unread characters in the buffers.
+Returns the number of unread characters in the selected buffer.
 */
 uint8_t uartCount(uint8_t buffer_type){
 	if(buffer_type == UART_BUFFER_TYPE_TX){
@@ -271,7 +295,7 @@ uint8_t uartCount(uint8_t buffer_type){
 }
 
 /*
-Returns the number of unread characters in the buffers.
+Returns the number of unread characters in the selected buffer.
 */
 uint8_t uartIsFull(uint8_t buffer_type){
 	if(buffer_type == UART_BUFFER_TYPE_TX){
@@ -293,7 +317,7 @@ uint8_t uartIsFull(uint8_t buffer_type){
 }
 
 /*
-Returns the last character in the buffers.
+Returns the last character in the selected buffer.
 */
 uint8_t uartPeek(uint8_t buffer_type, int *data){
 	if(buffer_type == UART_BUFFER_TYPE_TX){
@@ -316,13 +340,14 @@ uint8_t uartPeek(uint8_t buffer_type, int *data){
 }
 
 /*
-Returns a single char received and stored in the RX buffer. Used in conjunction with 'uart_available()'.
+Returns a single char received and stored in the Rx buffer. Used in conjunction with 'uartAvailable()'.
 Pops the read character out of the buffer. 
 */
 char uartRead(){
 	char data = 0x00;
 	//Return popped data only if the pop operation is successful.
-	if(uartBufferPop(UART_BUFFER_TYPE_RX, &data) == UART_RX_BUFFER_OK){
+	if(uartAvailable(UART_BUFFER_TYPE_RX)){
+		uartBufferPop(UART_BUFFER_TYPE_RX, &data);
 		return data;
 	}else{
 		return 0x00;
@@ -330,20 +355,9 @@ char uartRead(){
 }
 
 /*
-(Deprecated) Waits in a loop till a char is received. Has no relationship with the buffer based Rx mechanism.
-*/
-char uartReadCharWait(){
-	//Waits till a character is received in a while loop.
-	usart_led_toggle== USART_LED_ON ? (PORTD|= 0x04) : (PORTD&= ~0x04);
-	while(!(UCSR0A & (1<< RXC0))); //Check the status of RXC0.
-	usart_led_toggle== USART_LED_ON ? (PORTD&= ~0x04) : (PORTD&= ~0x04);	//Turn off TX LED. Used for resiliency against turning off the LEDs within interrupts.
-	return UDR0;
-}
-
-/*
 Detects a frame error in data reception.
 */
-uint8_t uartFrameEror(){
+uint8_t uartFrameError(){
 	if(UCSR0A & UART_FRAME_ERROR){
 		return UART_FRAME_ERROR;
 	}else{
@@ -352,14 +366,28 @@ uint8_t uartFrameEror(){
 }
 
 /*
+Returns the frame error count. The value rolls over after 255.
+*/
+uint8_t uartGetFrameErrorCount(){
+	return _rx_frame_errors;
+}
+
+/*
 Detects a data overrun error in data reception.
 */
-uint8_t uartDataOverrunError(){
+uint8_t uartOverrunError(){
 	if(UCSR0A & UART_DATA_OVERRUN_ERROR){
 		return UART_DATA_OVERRUN_ERROR;
 	}else{
 		return UART_OK;
 	}
+}
+
+/*
+Returns the overrun error count. The value rolls over after 255.
+*/
+uint8_t uartGetOverrunErrorCount(){
+	return _rx_overrun_errors;
 }
 
 /*
@@ -371,6 +399,37 @@ uint8_t uartParityError(){
 	}else{
 		return UART_OK;
 	}
+}
+
+/*
+Returns the parity error count. The value rolls over after 255.
+*/
+uint8_t uartGetParityErrorCount(){
+	return _rx_parity_errors;
+}
+
+/*
+(Deprecated) Transmits a single character on the Tx line using polling. Has no relationship with the buffer based Tx mechanism.
+*/
+void uartSendChar(char data){
+	usart_led_toggle == USART_LED_ON ? (PORTD |= UART_TX_LED) : (PORTD &= ~UART_TX_LED);		//Turn on the Tx LED.
+	
+	while(!(UCSR0A & UART_DATA_REGISTER_EMPTY)); //Check the status of UDRE0. Transmit only when ready.
+	UDR0= data;	//Write data to the register.
+	//while(!(UCSR0A & (1 << TXC0))); //Wait till the shift register has shifted out all bits.
+	
+	usart_led_toggle == USART_LED_ON ? (PORTD &= ~UART_TX_LED) : (PORTD &= ~UART_TX_LED);	//Turn off TX LED. Used for resiliency against turning off the LEDs within interrupts.
+}
+
+/*
+(Deprecated) Waits in a loop till a char is received. Has no relationship with the buffer based Rx mechanism.
+*/
+char uartReadCharWait(){
+	//Waits till a character is received in a while loop.
+	usart_led_toggle== USART_LED_ON ? (PORTD|= 0x04) : (PORTD&= ~0x04);
+	while(!(UCSR0A & (1<< RXC0))); //Check the status of RXC0.
+	usart_led_toggle== USART_LED_ON ? (PORTD&= ~0x04) : (PORTD&= ~0x04);	//Turn off TX LED. Used for resiliency against turning off the LEDs within interrupts.
+	return UDR0;
 }
 
 
--- Libraries/UART/UART/uart.h
+++ Libraries/UART/UART/uart.h
@@ -1,35 +1,41 @@
 /*
- * usart328P.h
+ * uart328P.h
  *
  * Created: 30-Oct-18 6:54:33 PM
  * Author: Ranul Deepanayake.
- * UART library for the ATmega328P. Supports character and string transmission, reception, character reception, selectable baud rate and reception error detection.
- * Has a re-sizable RX buffer.
- * Supports 5- 8 bit data frames.
+ * UART library for the ATmega328P. Supports character transmission, reception, selectable baud rates, reception error detection and activity indication.
+ * Supports baud rates of 9600- 57600 (other values may work but have not been tested).
+ * Circular buffer and interrupt based transmission and reception. Tx and Rx buffers sizes can be set individually. 
+ * Supports 5- 8 bit data frames. Development for 9 bit data frames is pending.
+ * Supports parities of none, even and odd.
+ * Supports one and two stop bits.
+ * Supports frame error, overrun error and parity error detection with free running error counters.
+ * Supports Tx/Rx activity indicator LEDs which use PORTD. Default pins; Rx- PORTD2 and Tx- PORTD3.
  */ 
-
 
 #ifndef USART328P_H_
 #define USART328P_H_
-
-//Includes.
-#include <avr/io.h>
-#include <avr/interrupt.h>
 
 //CPU frequency definition.
 #ifndef F_CPU
 #define F_CPU 16000000UL
 #endif
-
+//Tx LED.
+#ifndef UART_TX_LED
+#define UART_TX_LED 0x04
+#endif
+//Rx LED.
+#ifndef UART_RX_LED
+#define UART_RX_LED 0x08
+#endif
+//Calculations.
 #define UART_BAUD_RATE(BAUD_RATE) (((F_CPU)/(BAUD_RATE* 16UL))- 1)
-#define UART_NULL_CHARACTER 0x00
-#define UART_CARRIAGE_RETURN 0x0D
-#define UART_NEW_LINE 0x0A
-
-#define UART_RX_COMPLETE 0x80
-#define UART_TX_COMPLETE 0x40
-
-//Various UART modes.
+//Buffer modifiers.
+#define UART_BUFFER_TYPE_TX 0
+#define UART_BUFFER_TYPE_RX 1
+#define UART_TX_BUFFER_SIZE 64
+#define UART_RX_BUFFER_SIZE 64
+//Various UART operational modes.
 #define UART_TX_ENABLE 0x08
 #define UART_RX_ENABLE 0x10
 #define UART_DATA_REGISTER_EMPTY_INTERRUPT_ENABLE 0x20
@@ -37,31 +43,17 @@
 #define UART_RX_INTERRUPT_ENABLE 0x80
 #define UART_2X_MODE 0x02
 #define UART_ASYNCHRONOUS_MODE 0x00
-#define UART_DATA_SIZE_5 0x00;
-#define UART_DATA_SIZE_6 0x02;
-#define UART_DATA_SIZE_7 0x04;
-#define UART_DATA_SIZE_8 0x06;
-//#define UART_DATA_SIZE_9 UCSR0C|= 0x06; UCSR0B|= 0x04;
-//LEDs.
-#ifndef UART_TX_LED
-#define UART_TX_LED 0x02
-#endif
-
-#ifndef UART_RX_LED
-#define UART_RX_LED 0x01
-#endif
-
-//Flags.
-#define UART_DATA_REGISTER_EMPTY 0x20
-
-//Parity and stop bits selection.
-#define UART_PARITY_NONE 0x00 
+#define UART_DATA_SIZE_5 0x00
+#define UART_DATA_SIZE_6 0x02
+#define UART_DATA_SIZE_7 0x04
+#define UART_DATA_SIZE_8 0x06
+#define UART_PARITY_NONE 0x00
 #define UART_PARITY_EVEN 0x20
 #define UART_PARITY_ODD 0x30
 #define UART_STOP_BITS_1 0x00
 #define UART_STOP_BITS_2 0x08
-
-//Error and status codes.
+//#define UART_DATA_SIZE_9 0x08 //Need to configure two registers. This value is for differentiation only, and is not the value of either of the registers.
+//Status and error flags.
 #define UART_FRAME_ERROR 0x10
 #define UART_DATA_OVERRUN_ERROR 0x08
 #define UART_PARITY_ERROR 0x04
@@ -72,54 +64,66 @@
 #define UART_RX_BUFFER_FULL 3
 #define UART_TX_BUFFER_EMPTY 4
 #define UART_RX_BUFFER_EMPTY 5
+#define UART_RX_COMPLETE 0x80
+#define UART_TX_COMPLETE 0x40
+#define UART_DATA_REGISTER_EMPTY 0x20
 #define UART_UNDEFINED_CONDITION 100
-
-#define UART_BUFFER_TYPE_TX 0
-#define UART_BUFFER_TYPE_RX 1
-#define UART_TX_BUFFER_SIZE 64
-#define UART_RX_BUFFER_SIZE 64
+//ASCII characters.
+#define UART_NULL_CHARACTER 0x00
+#define UART_CARRIAGE_RETURN 0x0D
+#define UART_NEW_LINE 0x0A
 
 /*
 Function definitions.
 */
 //Set up the UART peripheral.
 void uartSet(uint16_t baud_rate, uint8_t data_bits, uint8_t parity, uint8_t stop_bits);
+//Set up Tx and Rx LEDs. Uses PORTD.
 void uartSetLed(uint8_t toggle);
-//Transmit a single character on the Tx line.
-void uartSendChar(char data);
-//Send a string without carriage return and newline.
+//Transmit a string without carriage return and newline.
 void uartPrint(char *string_pointer);	
-//Send a string with carriage return and newline.
+//Transmit a string with carriage return and newline.
 void uartPrintLn(char *string_pointer);
-//Pushes a received character into the Rx buffer.
+//Push a character into the the selected buffer.
 uint8_t uartBufferPush(uint8_t buffer_type, char data);
-//Pops a received character from the Rx buffer.
+//Pop a character from the the selected buffer.
 uint8_t uartBufferPop(uint8_t buffer_type, char *data);
-//Returns the number of unread characters in the Rx buffer.
+//Return whether unread characters are present in the selected buffer.
 uint8_t uartAvailable(uint8_t buffer_type);
+//Return the number of unread characters in the selected buffer.
 uint8_t uartCount(uint8_t buffer_type);
+//Return whether the selected buffer is full.
 uint8_t uartIsFull(uint8_t buffer_type);
+//Read the last character from the selected buffer.
 uint8_t uartPeek(uint8_t buffer_type, int *data);
-//Read a character in the Rx buffer.
+//Read a character from the Rx buffer.
 char uartRead();
-//Wait till a character is received in the receive register.
-char uartReadCharWait();
 //Check for a frame error on reception.
 uint8_t uartFrameError();
+//Return the frame error count.
+uint8_t uartGetFrameErrorCount();
 //Check for a data overrun error on reception.
-uint8_t uartDataOverrunError();
+uint8_t uartOverrunError();
+//Return the overrun error count.
+uint8_t uartGetOverrunErrorCount();
 //Check for a parity on reception.
 uint8_t uartParityError();
+//Return the parity error count.
+uint8_t uartGetParityErrorCount();
+//(Deprecated) Transmit a single character.
+void uartSendChar(char data);
+//(Deprecated) Wait till a character is received in the receive register.
+char uartReadCharWait();
 
-
-//Example implementation.
 /*
+Example implementation.
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
 
 int main(){
-	uartSet(UART_BAUD_RATE(9600), 8, UART_PARITY_NONE, UART_STOP_BITS_1);	//Set up UART peripheral. 
+	uartSet(UART_BAUD_RATE(9600), UART_DATA_SIZE_8, UART_PARITY_NONE, UART_STOP_BITS_1);	//Set up UART peripheral. 
 	
 	char buffer[7];
 	uint8_t position= 0;
@@ -128,8 +132,8 @@ int main(){
 	while(1){
 		uartPrintLn("Sending message...");
 		
-		while(uartAvailable()> 0 && complete== 0){
-			char temp= uart_read();
+		while(uartAvailable() && complete== 0){
+			char temp= uartRead();
 			if(temp== '\r'){
 				buffer[position]= 0;
 				position= 0;
