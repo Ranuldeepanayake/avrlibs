78b13f80fed4fc38f0ecca650cc66f3d75e97b35
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2024-07-27 01:02:18 +0530

    ADC library rewrite

--- Libraries/ADC/ADC/adc.c
+++ Libraries/ADC/ADC/adc.c
@@ -5,45 +5,193 @@
  * Author: Ranul Deepanayake
  */ 
 
+
 #include "adc.h"
 
+#ifdef ADC_DEBUG
+	#include <stdlib.h>
+	#include <string.h>
+	#include "uart.h"
+#endif
+
+#if ADC_PACKAGE == ADC_PACKAGE_PDIP
+	volatile uint16_t _adc_results[ADC_CHANNEL_COUNT] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //Initialize the array.
+#elif ADC_PACKAGE != ADC_PACKAGE_PDIP
+	volatile uint16_t _adc_results[ADC_CHANNEL_COUNT] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //Initialize the array.
+#endif
+
+volatile uint8_t _adc_channel_index = ADC_CHANNEL_0, _adc_vref_temp = 0;
+uint8_t _adc_mode = ADC_MODE_SINGLE_CONVERSION;
+
 /*
-Set up the ADC peripheral.
+Set the ADC peripheral.
 */
-void adc_set(uint8_t reference_voltage, uint8_t interrupt_enable, uint8_t prescaler){
-	ADMUX|= reference_voltage;
-	ADCSRA|= ((1<< ADEN) | interrupt_enable | prescaler); //Enable the ADC and set pre-scaler.
+void adcSet(uint8_t mode, uint8_t channel, uint8_t prescaler, uint8_t vref){
+	//Save the vref value to a global variable to be used within the ISR for safely changing the MUX bits.
+	_adc_vref_temp = vref;
+	
+	if(mode == ADC_MODE_SINGLE_CONVERSION){
+		//Free running mode.
+		_adc_mode = ADC_MODE_SINGLE_CONVERSION;
+		ADMUX |= (vref | ADC_CHANNEL_0); //Select the voltage reference and channel 0 for the initial reading.
+		ADCSRA |= ((1 << ADEN) | prescaler); //Enable the ADC, enable triggers, set pre-scalers and enable interrupts.
+		ADCSRA |= (1 << ADSC); //Start free running conversion. If ADSC is enabled at the same time as ADEN, it will take 25 ADC cycles instead of 13.
+		
+		#if ADC_DEBUG >= 1
+			uartPrint("[ADC][adcSet][mode][Single conversion]");
+		#endif
+		
+	}else if(mode == ADC_MODE_FREE_RUNNING){
+		_adc_mode = ADC_MODE_FREE_RUNNING;
+		//Do stuff.
+	}
 }
 
 /*
 Return a 10 bit ADC reading in single conversion mode.
 */
-uint16_t adc_read(uint8_t channel){
-	//Single conversion mode.
-	adc_disable_digital_buffer(channel);
-	ADMUX&= ADC_CHANNEL_DESELECT; //Clear previously set channels.
-	ADMUX|= channel; //Set the channel.
-	ADCSRA|= ADC_SINGLE_CONVERSION_PENDING; //Start single conversion.
-	while(ADCSRA & ADC_SINGLE_CONVERSION_PENDING); //Wait for conversion to complete (for bit to become 0).
+uint16_t adcRead(uint8_t channel){
+	//If no conversions are pending.
+	if(!(ADCSRA & (1 << ADSC))){
+		
+		//ADCW must be read as early as possible.
+		//The next conversion starts the moment the ISR is entered.
+		//After a MUX change, the corresponding ADCW value will be captured only in the next ISR cycle.
+
+		_adc_results[_adc_channel_index] = ADCW; //Read the previous other than for the first reading.
+		
+		//Increment the channel index to read the next ADC channel after the ISR exits.
+		_adc_channel_index++;
+		
+		//Roll over to channel 1. This has to be done before the ISR ends.
+		if(_adc_channel_index == ADC_CHANNEL_COUNT){
+			_adc_channel_index = ADC_CHANNEL_0;
+		}
+		
+		#if ADC_PACKAGE == ADC_PACKAGE_PDIP
+			//Set the new channel. The following conditional checker is needed since ADC channel numbers are not contiguous from channel 6.
+			if(_adc_channel_index <= ADC_CHANNEL_5){
+				ADMUX = (_adc_vref_temp | _adc_channel_index);
+				
+			}else if(_adc_channel_index > ADC_CHANNEL_5){
+				switch (_adc_channel_index){
+					case 6:
+					//Unset the previous channel.
+						ADMUX = (_adc_vref_temp | ADC_CHANNEL_TEMPERATURE_SENSOR);
+						break;
+					case 7:
+						//Unset the previous channel.
+						ADMUX = (_adc_vref_temp | ADC_CHANNEL_BANDGAP);
+						break;
+					case 8:
+						//Unset the previous channel.
+						ADMUX = (_adc_vref_temp | ADC_CHANNEL_GROUND);
+						break;
+				}
+			}
+			
+		#elif ADC_PACKAGE != ADC_PACKAGE_PDIP
+			//Set the new channel. The following conditional checker is needed since ADC channel numbers are not contiguous from channel 6.
+			if(_adc_channel_index <= ADC_CHANNEL_7){
+				ADMUX = (_adc_vref_temp | _adc_channel_index);
+				
+			}else if(_adc_channel_index > ADC_CHANNEL_7){
+				switch (_adc_channel_index){
+					case 8:
+						//Unset the previous channel.
+						ADMUX = (_adc_vref_temp | ADC_CHANNEL_TEMPERATURE_SENSOR);
+						break;
+					case 9:
+						//Unset the previous channel.
+						ADMUX = (_adc_vref_temp | ADC_CHANNEL_BANDGAP);
+						break;
+					case 10:
+						//Unset the previous channel.
+						ADMUX = (_adc_vref_temp | ADC_CHANNEL_GROUND);
+						break;
+				}
+			}
+		#endif
+		
+		ADCSRA |= (1 << ADSC); //Restart conversion.
+	}
 	
-	uint16_t value= ADCL;	//Read low byte first.
-	value|= (ADCH<< 8);
+	#if ADC_DEBUG >= 1
+		char temp[5], temp_2[5];
+		adcDebugIntToHex(channel, temp); adcDebugIntToHex(_adc_results[channel], temp_2);
+		uartPrint("[ADC][read][CH]["); uartPrint(temp); uartPrint("]["); uartPrint(temp_2); uartPrintLn("]");
+	#endif
+	
+	return _adc_results[channel];
+}
+
+
+/*
+Return a 10 bit ADC reading in single conversion mode.
+*/
+uint16_t adcReadWait(uint8_t channel){
+	//Do not change the channel if a conversion is in progress.
+	if(ADCSRA & (1<< ADSC)){
+		return 0;
+	}
+	
+	ADMUX&= ~ADC_CHANNEL_UNSET; //Clear previously set channels.
+	
+	ADMUX |= channel; //Set the channel.
+	ADCSRA |= (1<< ADSC); //Start single conversion.
+	while(ADCSRA & (1<< ADSC)); //Wait for conversion to complete (for bit to become 0).
+	
+	//Read the value.
+	uint16_t value = ADCW;
+	
+	#if ADC_DEBUG >= 1
+		char temp[5];
+		adcDebugIntToHex(value, temp);
+		uartPrint("[ADC][readWait][val]["); uartPrint(temp); uartPrintLn("]");
+	#endif
+	
 	return value;
 }
 
-/*
-Disable the digital input buffer of a channel for power saving (not required).
-*/
-void adc_disable_digital_buffer(uint8_t channel){
-	DIDR0&= ~channel;
+uint16_t adcReadTemperatureSensor(){
+	uint16_t temperature = 0;
+	
+	uint16_t raw_value = adcRead(ADC_TEMP);
+
+	//Temperature calculation in celcius.
+	temperature = ((uint16_t)((raw_value- 242)* (85- (-45))/ (380- 242)+ (-45)))- 35;
+	
+	#if ADC_DEBUG >= 1
+		char temp[5];
+		adcDebugIntToHex(temperature, temp);
+		uartPrint("[ADC][readTempSensor][C]["); uartPrint(temp); uartPrintLn("]");
+	#endif
+	
+	return temperature;
 }
 
-uint16_t adc_read_temperature_sensor(){
-	uint16_t raw_value= 0;
-	
-	adc_set(ADC_AREF_INTERNAL_BANDGAP, ADC_INTERRUPT_DISABLE, ADC_PRESCALER_128);
-	raw_value= adc_read(ADC_CHANNEL_TEMPERATURE_SENSOR);
-	
-	return ((uint16_t)((raw_value- 242)* (85- (-45))/ (380- 242)+ (-45)))- 35;
+/*
+Disable the digital input buffer of an ADC channel for power saving (not required).
+*/
+void adcDisableDigitalInputBuffer(uint8_t channel){
+	DIDR0 |= channel; //Write a 1 to disable.
 }
+
+/*
+Enable the digital input buffer of an ADC channel if it was disabled for power saving.
+*/
+void adcEnableDigitalInputBuffer(uint8_t channel){
+	DIDR0 &= ~channel; //Write a 0 to enable.
+}
+
+/*
+Supporting function which converts numbers in the decimal format to hexadecimal format when storing as a string.
+*/
+void adcDebugIntToHex(uint16_t input, char *temp_1){
+	memset(temp_1, 0, sizeof(&temp_1));
+	itoa(input, temp_1, 10);
+}
+
+
+
 
--- Libraries/ADC/ADC/adc.h
+++ Libraries/ADC/ADC/adc.h
@@ -17,24 +17,40 @@
 //Includes.
 #include <avr/io.h>
 
-//Attributes.
+//Debug level.
+#ifndef ADC_DEBUG
+	#define ADC_DEBUG 2
+#endif
+//ADC pin numbers.
+#define ADC_0 0
+#define ADC_1 1
+#define ADC_2 2
+#define ADC_3 3
+#define ADC_4 4
+#define ADC_5 5
+#if ADC_PACKAGE == ADC_PACKAGE_PDIP
+	#define ADC_TEMP 6
+	#define ADC_BGP 7
+	#define ADC_GND 8
+#elif ADC_PACKAGE != ADC_PACKAGE_PDIP
+	#define ADC_6 6
+	#define ADC_7 7
+	#define ADC_TEMP 8
+	#define ADC_BGP 9
+	#define ADC_GND 10
+#endif
+//Physical package type. Needed to set the appropriate number of channels.
 #define ADC_PACKAGE_PDIP 0
 #define ADC_PACKAGE_TQFP 1
 #define ADC_PACKAGE_VFQFN 2
 #define ADC_PACKAGE ADC_PACKAGE_PDIP
-
-#define ADC_AREF_EXTERNAL 0x00
-#define ADC_AREF_AVCC 0x40
-#define ADC_AREF_INTERNAL_BANDGAP 0xC0
-#define ADC_INTERRUPT_ENABLE 0x08
-#define ADC_INTERRUPT_DISABLE 0x00
-#define ADC_PRESCALER_2 0x01
-#define ADC_PRESCALER_4 0x02
-#define ADC_PRESCALER_8 0x03
-#define ADC_PRESCALER_16 0x04
-#define ADC_PRESCALER_32 0x05
-#define ADC_PRESCALER_64 0x06
-#define ADC_PRESCALER_128 0x07
+//Channel count (depends on the physical package).
+#if ADC_PACKAGE == ADC_PACKAGE_PDIP
+	#define ADC_CHANNEL_COUNT 0x09
+#elif ADC_PACKAGE != ADC_PACKAGE_PDIP
+	#define ADC_CHANNEL_COUNT 0x0B
+#endif
+//Channel definition.
 #define ADC_CHANNEL_0 0x00
 #define ADC_CHANNEL_1 0x01
 #define ADC_CHANNEL_2 0x02
@@ -44,23 +60,62 @@
 #define ADC_CHANNEL_TEMPERATURE_SENSOR 0x08
 #define ADC_CHANNEL_BANDGAP 0x0E
 #define ADC_CHANNEL_GROUND 0x0F
-#if ADC_PACKAGE!= ADC_PACKAGE_PDIP
-#define ADC_CHANNEL_6 0x06
-#define ADC_CHANNEL_7 0x07
+//Other packages will have two additional channels.
+#if ADC_PACKAGE != ADC_PACKAGE_PDIP
+	#define ADC_CHANNEL_6 0x06
+	#define ADC_CHANNEL_7 0x07
 #endif
-#define ADC_CHANNEL_DESELECT 0xF0
-
-//Status and error codes.
-#define ADC_SINGLE_CONVERSION_PENDING 0x40
+//Voltage reference sources.
+#define ADC_VREF_EXTERNAL 0x00
+#define ADC_VREF_AVCC 0x40
+#define ADC_VREF_INTERNAL 0xC0
+//Prescaler values.
+#define ADC_PRESCALER_2 0x01
+#define ADC_PRESCALER_4 0x02
+#define ADC_PRESCALER_8 0x03
+#define ADC_PRESCALER_16 0x04
+#define ADC_PRESCALER_32 0x05
+#define ADC_PRESCALER_64 0x06
+#define ADC_PRESCALER_128 0x07 //Most suitable for free running mode to reduce the number of interrupts.
+//ADC modes.
+#define ADC_MODE_SINGLE_CONVERSION 0
+#define ADC_MODE_FREE_RUNNING 1
+//Options.
+#define ADC_TRIGGER_FREE_RUNNING 0x00
+#define ADC_INTERRUPT_ENABLE 0x08
+#define ADC_INTERRUPT_DISABLE 0x00
+#define ADC_CHANNEL_UNSET 0x0F
 
 //Functions.
 //Set up the ADC peripheral.
-void adc_set(uint8_t reference_voltage, uint8_t interrupt_enable, uint8_t prescaler);
+void adcSet(uint8_t mode, uint8_t channel, uint8_t prescaler, uint8_t vref);
+//Return a 10 bit ADC reading in free-running mode.
+uint16_t adcRead(uint8_t channel);
 //Return a 10 bit ADC reading in single conversion mode.
-uint16_t adc_read(uint8_t channel);
-//Disable the digital input buffer of a channel for power saving.
-void adc_disable_digital_buffer(uint8_t channel);
+uint16_t adcReadWait(uint8_t channel);
 //Return the temperature from the internal temperature sensor (in Celcius).
-uint16_t adc_read_temperature_sensor();
+uint16_t adcReadTemperatureSensor();
+//Disable the digital input buffer of a channel for power saving.
+void adcDisableDigitalInputBuffer(uint8_t channel);
+//Enable the digital input buffer of a channel.
+void adcEnableDigitalInputBuffer(uint8_t channel);
+void adcDebugIntToHex(uint16_t input, char *temp_1);
+
+/*
+
+Example implementation.
+
+int main(void)
+{
+	adcSet(ADC_MODE_FREE_RUNNING, ADC_CHANNEL_0, ADC_PRESCALER_128, ADC_VREF_AVCC);
+	adcDisableDigitalInputBuffer(0x02); //Optional for power saving.
+	
+	while (1)
+	{
+		uint8_t = adcRead(ADC_CHANNEL_1);
+	}
+}
+
+*/
 
 #endif /* ADC_H_ */
